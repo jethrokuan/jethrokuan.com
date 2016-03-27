9ac6596167e666947083d6b07cbfa1836f3d6303
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-27 13:14:53 +0800

    Updated site

---
+++ sass/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((firestarter . "sassc -m -t compressed main.scss ../css/styles.css"))))
