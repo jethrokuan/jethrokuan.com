f0785877db7ed60c0bcd6aa627e723d534332306
Jethro Kuan <jethrokuan95@gmail.com>
2016-04-02 15:09:10 +0800

    Add books

--- sass/.dir-locals.el
+++ sass/.dir-locals.el
@@ -1,5 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil .
-      ((firestarter . "sassc -m -t compressed main.scss ../css/styles.css"))))
+((nil
+  (firestarter . "sassc -m -t compressed main.scss ../css/styles.css")))
+
